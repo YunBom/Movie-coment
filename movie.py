616129180ae987b9549cb08c433532b87f3c0c61
Yun Bom Yi <ahybi986@naver.com>
2023-01-05 15:46:22 +0900

    페이지 넘기기 작업

---
+++ movie.py
@@ -0,0 +1,12 @@
+import selenium
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+import time
+
+from wordcloud import wordCloud
+import matplotlib.pyplot as plt
+from collections import Counter
+from konlpy.tag import Okt
+from PIL import Image
+import numpy as np
+
